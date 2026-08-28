8459f8e24e0f6b9d7d4ebcf6f8a02c999c8e68a8
MGK-onsta1 <mingos.g.konstantinos@gmail.com>
2026-08-28 16:50:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+namespace Ratespiel_umgedreht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int tipp = rnd.Next(1, 101);
+            Console.WriteLine($"Ist es {tipp}?");
+            string antwort = Console.ReadLine();
+           
+            if (antwort.ToLower().Contains("tiefer"))
+            {
+                Console.WriteLine("Okay, tiefer.");
+            }
+            else if (antwort.ToLower().Contains("höher"))
+            {
+                Console.WriteLine("Okay, höher.");
+            }
+            else if (antwort.ToLower().Contains("richtig"))
+            {
+                Console.WriteLine("Ich hab's!");
+            }
+            else
+                Console.WriteLine("Das hab ich nicht verstanden!");
+        }  
+
+    }
+}
