5fcbb66376ed1c1e1e67ffe7d439f1f5b820d272
MGK-onsta1 <mingos.g.konstantinos@gmail.com>
2026-09-11 14:24:59 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,46 @@
-﻿namespace Ratespiel
+﻿using System.ComponentModel.Design;
+
+namespace Ratespiel_umgedreht
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int zahl = rnd.Next(1, 101);
-            int tipp;
+            int min = 1;
+            int max = 100;
+            string antwort;  
             int versuche = 0;
 
-            Console.WriteLine("Ich habe eine Zahl zwischen 1 und 100 gewählt");
             do
             {
-                Console.WriteLine("Dein Tipp: ");
-                tipp = Convert.ToInt32(Console.ReadLine());
+                int tipp = rnd.Next(min, max + 1);
                 versuche++;
+                Console.WriteLine($"Ist es {tipp}?");
+                antwort = Console.ReadLine().ToLower();
 
-                if (tipp < zahl)
-                    Console.WriteLine("zu klein");
-                else if (tipp > zahl)
-                    Console.WriteLine("zu gross");
+                if (antwort.Contains("tiefer"))
+                {
+                    Console.WriteLine("Okay, tiefer.");
+                    max = tipp - 1;
+                }
+                else if (antwort.Contains("höher"))
+                {
+                    Console.WriteLine("Okay, höher.");
+                    min = tipp + 1;
+                }
+                else if (antwort.Contains("richtig"))
+                {
+                    Console.WriteLine("Ich hab's!");
+                }
                 else
-                    Console.WriteLine($"Richtig! Du hast {versuche} Versuche gebraucht");
-            } while (tipp != zahl);
-                                 
+                {
+                    Console.WriteLine("Das hab ich nicht verstanden!");
+                }
+
+            } while (!antwort.Contains("richtig"));
+
+            Console.WriteLine($"Ich habe {versuche} Versuche gebraucht!");
         }
     }
 }
