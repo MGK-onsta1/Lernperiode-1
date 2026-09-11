bc3cfb760374e57f49c2501f507b53487c2dcf43
MGK-onsta1 <mingos.g.konstantinos@gmail.com>
2026-09-11 14:26:36 +0200

    Add files via upload

---
+++ Program.Ratespiel.cs
@@ -0,0 +1,29 @@
+﻿namespace Ratespiel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int zahl = rnd.Next(1, 101);
+            int tipp;
+            int versuche = 0;
+
+            Console.WriteLine("Ich habe eine Zahl zwischen 1 und 100 gewählt");
+            do
+            {
+                Console.WriteLine("Dein Tipp: ");
+                tipp = Convert.ToInt32(Console.ReadLine());
+                versuche++;
+
+                if (tipp < zahl)
+                    Console.WriteLine("zu klein");
+                else if (tipp > zahl)
+                    Console.WriteLine("zu gross");
+                else
+                    Console.WriteLine($"Richtig! Du hast {versuche} Versuche gebraucht");
+            } while (tipp != zahl);
+                                 
+        }
+    }
+}
