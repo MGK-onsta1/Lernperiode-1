522c195ad030faafc03a99d34264ad7897f0ebed
MGK-onsta1 <mingos.g.konstantinos@gmail.com>
2026-09-11 14:23:38 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,46 +1,29 @@
-﻿using System.ComponentModel.Design;
-
-namespace Ratespiel_umgedreht
+﻿namespace Ratespiel
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int min = 1;
-            int max = 100;
-            string antwort;  
+            int zahl = rnd.Next(1, 101);
+            int tipp;
             int versuche = 0;
 
+            Console.WriteLine("Ich habe eine Zahl zwischen 1 und 100 gewählt");
             do
             {
-                int tipp = rnd.Next(min, max + 1);
+                Console.WriteLine("Dein Tipp: ");
+                tipp = Convert.ToInt32(Console.ReadLine());
                 versuche++;
-                Console.WriteLine($"Ist es {tipp}?");
-                antwort = Console.ReadLine().ToLower();
 
-                if (antwort.Contains("tiefer"))
-                {
-                    Console.WriteLine("Okay, tiefer.");
-                    max = tipp - 1;
-                }
-                else if (antwort.Contains("höher"))
-                {
-                    Console.WriteLine("Okay, höher.");
-                    min = tipp + 1;
-                }
-                else if (antwort.Contains("richtig"))
-                {
-                    Console.WriteLine("Ich hab's!");
-                }
+                if (tipp < zahl)
+                    Console.WriteLine("zu klein");
+                else if (tipp > zahl)
+                    Console.WriteLine("zu gross");
                 else
-                {
-                    Console.WriteLine("Das hab ich nicht verstanden!");
-                }
-
-            } while (!antwort.Contains("richtig"));
-
-            Console.WriteLine($"Ich habe {versuche} Versuche gebraucht!");
+                    Console.WriteLine($"Richtig! Du hast {versuche} Versuche gebraucht");
+            } while (tipp != zahl);
+                                 
         }
     }
 }
