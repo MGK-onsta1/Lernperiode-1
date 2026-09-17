c2fe490e1e020510174aa9c9ee674c3223570030
MGK-onsta1 <mingos.g.konstantinos@gmail.com>
2026-09-17 21:17:30 +0200

    Add files via upload

---
+++ Program.Rechner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string antwort = "j";
+
+        
+        while (antwort == "j")
+        {
+            Console.Clear();
+            Console.WriteLine("--- Einfacher Taschenrechner ---");
+
+            
+            Console.Write("Erste Zahl: ");
+            double zahl1 = Convert.ToDouble(Console.ReadLine());
+
+           
+            Console.Write("Operator (+, -, *, /): ");
+            string op = Console.ReadLine();
+
+            
+            Console.Write("Zweite Zahl: ");
+            double zahl2 = Convert.ToDouble(Console.ReadLine());
+
+            double ergebnis = 0;
+
+            
+            if (op == "+")
+            {
+                ergebnis = zahl1 + zahl2;
+            }
+            else if (op == "-")
+            {
+                ergebnis = zahl1 - zahl2;
+            }
+            else if (op == "*")
+            {
+                ergebnis = zahl1 * zahl2;
+            }
+            else if (op == "/")
+            {
+                ergebnis = zahl1 / zahl2;
+            }
+
+          
+            Console.WriteLine("Ergebnis: " + ergebnis);
+
+            
+            Console.Write("\nNochmal rechnen? (j/n): ");
+            antwort = Console.ReadLine();
+        }
+
+        Console.WriteLine("Tschüss!");
+    }
+}
