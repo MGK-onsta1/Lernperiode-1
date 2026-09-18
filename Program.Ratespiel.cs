72fe70880b7420c493d1718c839c032a3ee21dec
MGK-onsta1 <mingos.g.konstantinos@gmail.com>
2026-09-18 15:34:19 +0200

    Update Program.Ratespiel.cs

--- Program.Ratespiel.cs
+++ Program.Ratespiel.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int zahl = rnd.Next(1, 101);
+            int geheimeZahl = rnd.Next(1, 101);
             int tipp;
             int versuche = 0;
 
@@ -16,13 +16,13 @@
                 tipp = Convert.ToInt32(Console.ReadLine());
                 versuche++;
 
-                if (tipp < zahl)
+                if (tipp < geheimeZahl)
                     Console.WriteLine("zu klein");
-                else if (tipp > zahl)
+                else if (tipp > geheimeZahl)
                     Console.WriteLine("zu gross");
                 else
                     Console.WriteLine($"Richtig! Du hast {versuche} Versuche gebraucht");
-            } while (tipp != zahl);
+            } while (tipp != geheimeZahl);
                                  
         }
     }
